59cbabc6a8075b8f2c8d928abecbdfcbb335d712
Tusharsainirox <tusharsainirox.com>
2025-03-10 22:52:52 +0530

    backend CURD

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -79,3 +79,78 @@ export const getTask = asyncHandler(async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+//Update task
+export const updateTask = asyncHandler(async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id } = req.params;
+
+    if (!id) {
+      res.status(400).json({ message: "Please provide a Task Id to update the task" });
+    }
+
+    const task = await TaskModel.findById(id);
+
+    if(!task) {
+      res.status(400).json({ message: "Task not found" });
+    }
+    //Check if the user is authorized to update the task
+    if (!task.user.equals(userId)) {
+      res
+        .status(400)
+        .json({ message: "You are not authorized to view this task" });
+    }
+
+    const { title, description, dueDate, status, priority, completed } = req.body;
+
+    //update the task with new details if provided
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.dueDate = dueDate || task.dueDate;
+    task.status = status || task.status;
+    task.priority = priority || task.priority;
+    task.completed = completed || task.completed;
+
+    await task.save();
+
+    res.status(200).json({ task });
+
+  } catch (err) {
+    console.log({ message: err.message });
+    res.status(400).json({ message: "Task not updated" });
+  }
+});
+
+//Delete task
+export const deleteTask = asyncHandler(async (req, res) => { 
+
+  try {
+    const userId = req.user._id;
+    const { id } = req.params;
+
+    if (!id) {
+      res.status(400).json({ message: "Please provide a Task Id to delete the task" });
+    }
+
+    const task = await TaskModel.findById(id);
+
+    if(!task) {
+      res.status(400).json({ message: "Task not found" });
+    }
+
+    //Check if the user is authorized to delete the task
+    if (!task.user.equals(userId)) {
+      res.status(400).json({ message: "You are not authorized to delete this task" });
+    }
+
+    await TaskModel.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Task deleted successfully" });
+
+  } catch (err) {
+    console.log({ message: err.message });
+    res.status(400).json({ message: "Task not deleted" });
+  }
+
+});
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -3,6 +3,8 @@ import {
   createTask,
   getTasks,
   getTask,
+  updateTask,
+  deleteTask
 } from "../controllers/task/taskController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -10,4 +12,6 @@ const router = express.Router();
 router.post("/task/create", protect, createTask);
 router.get("/task/get", protect, getTasks);
 router.get("/task/get/:id", protect, getTask);
+router.patch("/task/update/:id", protect, updateTask);
+router.delete("/task/delete/:id", protect, deleteTask);
 export default router;
